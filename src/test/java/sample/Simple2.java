7f271ac70bbf9f85d461959524254a3b1c436f09
SweetiBassi <er.sweeti1987@gmail.com>
2022-07-25 16:10:12 +0530

    my

---
+++ src/test/java/sample/Simple2.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Simple2 {
+
+	public static void main(String[] args) {
+System.out.println("bye");
+System.out.println("hieee");
+	}
+
+}
---
+++ src/test/java/sample/simpletest.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class simpletest {
+
+	public static void main(String[] args) {
+System.out.println("hiiiiiiiiii");
+System.out.println("byeee");
+System.out.println("hello34");
+System.out.println("helloytr");
+
+	}
+//added something
+
+}
